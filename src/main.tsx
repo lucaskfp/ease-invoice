08f700835a58f18cfcb37af4ae49d3d008a9c6ee
Lucas Kennedy <lukakfp@gmail.com>
2023-11-11 13:51:18 -0300

    chore: Remove chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
 import App from "./App.tsx";
-import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import theme from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>
 );
