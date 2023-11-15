91595daae3f58ab26cb030a032c91b4f1b9e8500
Lucas Kennedy <lukakfp@gmail.com>
2023-11-15 11:02:20 -0300

    feat: Add custom primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,46 @@ export default {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              DEFAULT: "#2729ef",
+              foreground: "#ebf1ff",
+              50: "#ebf1ff",
+              100: "#b7c9ff",
+              200: "#a1b5ff",
+              300: "#8b9eff",
+              400: "#6370ff",
+              500: "#444afe",
+              600: "#2729ef",
+              700: "#1212cd",
+              800: "#05047e",
+              900: "#000033",
+            },
+          },
+        },
+        dark: {
+          colors: {
+            primary: {
+              DEFAULT: "#444afe",
+              foreground: "#ebf1ff",
+              50: "#000033",
+              100: "#04046b",
+              200: "#0b0ba3",
+              300: "#1616c1",
+              400: "#2425df",
+              500: "#444afe",
+              600: "#616fff",
+              700: "#8b9eff",
+              800: "#bccdff",
+              900: "#ebf1ff",
+            },
+          },
+        },
+      },
+    }),
+  ],
 };
