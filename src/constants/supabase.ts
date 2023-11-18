bd8a3d25cb17387f3ebed676c80a3abdc7812f5e
Lucas Kennedy <lukakfp@gmail.com>
2023-11-18 16:00:16 -0300

    feat: Config supabase client

--- src/constants/supabase.ts
+++ src/constants/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://ushaasutejfzfjpadufe.supabase.co";
-const supabaseKey = import.meta.env.SUPABASE_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
